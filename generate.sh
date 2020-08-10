aa3ac2e04349c794e67d7982be17201c5643a8a2
newtalentxp <newtalentxp@126.com>
2020-08-10 13:52:27 +0800

    fix

--- generate.sh
+++ generate.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-docker run --rm -v "${PWD}:/local" xopenapi/openapi-generator-cli:1.0.0 generate \
-    -i /local/openapi.yaml \
-    --git-repo-id storage-api-go \
-    --git-user-id xopenapi \
-    --package-name storage \
-    -g go \
-    -o /local/out/storage-api-go
+docker run --rm -v "${PWD}:/local" xopenapi/openapi-generator-cli:latest generate \
+	--auth signature \
+	--group-id com.xopenapi \
+	--artifact-id storage-api-java \
+	-i /local/openapi.yaml \
+	--git-repo-id storage-api-java \
+	--git-user-id xopenapi \
+	--api-package com.xopenapi.storage \
+	--model-package com.xopenapi.storage.model \
+	-g java \
+	-o /local/out/storage-api-java
