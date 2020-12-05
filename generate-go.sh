62715f0ff774117af3ef7edc95da01e57ddf9108
许盛 <fioman@qq.com>
2020-12-06 02:52:50 +0800

    x

--- generate-go.sh
+++ generate-go.sh
@@ -3,7 +3,7 @@ docker run --rm -v "${PWD}:/local" xopenapi/openapi-generator-cli:latest generat
 	--auth signature \
 	-i /local/openapi.yaml \
 	-g go \
-	--package-name=storage-api-go \
+	--package-name=storage \
 	--git-host=github.com \
 	--git-repo-id=storage-api-go \
 	--git-user-id=xopenapi \
